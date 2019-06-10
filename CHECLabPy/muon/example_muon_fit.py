c9c5029ddcda24f099b68ff1753b1783e5cc5205
Roberta Pillera <roberta.pillera@gmail.com>
2019-06-10 18:19:50 +0200

    updated example script with a scan over the whole simulation sample

--- CHECLabPy/muon/example_muon_fit.py
+++ CHECLabPy/muon/example_muon_fit.py
@@ -7,9 +7,25 @@ if __name__ == '__main__':
     
     dl1_path = "/lustrehome/pillera/CTA_CHEC/mc/Muons/run1_dl1.h5"
     reader = DL1Reader(dl1_path)
-    evnr = 0
-    image = reader[evnr]['photons'].values
-    a = reader.read("pointing")
-    alt = a['altitude_raw'][evnr]
-    azi = a['azimuth_raw'][evnr]
-    muon_evt = analyze_muon_event(image,reader.mapping,alt,azi)
+    
+    iev = reader.select_column('iev')
+    # evnr = 0
+    # image = reader[evnr]['photons'].values
+    # a = reader.read("pointing")
+    # alt = a['altitude_raw'][evnr]
+    # azi = a['azimuth_raw'][evnr]
+    # muon_evt = analyze_muon_event(image,reader.mapping,alt,azi)
+
+    index = []
+    for i in np.unique(iev):
+        image = reader[i]['photons'].values
+        a = reader.read("pointing")
+        alt = a['altitude_raw'][i]
+        azi = a['azimuth_raw'][i]
+        muon_evt = analyze_muon_event(image,reader.mapping,alt,azi)
+        if muon_evt[0] or muon_evt[1] is not None:
+            index.append(i)
+
+    print("Found "+str(len(index)+" muons")
+
+
