d0e95b676f05be9e21fe421f4f1ad16164b112db
Roberta Pillera <roberta.pillera@gmail.com>
2019-06-11 08:51:06 +0200

    Updated example script

--- CHECLabPy/muon/example_muon_fit.py
+++ CHECLabPy/muon/example_muon_fit.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from CHECLabPy.core.io import DL1Reader
 from muon_fitter import analyze_muon_event
+from astropy.table import Table
 
 if __name__ == '__main__':
     
@@ -18,6 +19,9 @@ if __name__ == '__main__':
     # muon_evt = analyze_muon_event(image,reader.mapping,alt,azi)
 
     index = []
+    par = {'Index': [],
+            'Size': [],
+            'RingComp': []}
     for i in np.unique(iev):
         if i%10 == 0:
             print(i)
@@ -27,8 +31,15 @@ if __name__ == '__main__':
         azi = a['azimuth_raw'][i]
         muon_evt = analyze_muon_event(image,reader.mapping,alt,azi)
         if muon_evt[0] or muon_evt[1] is not None:
-            index.append(i)
+            par['Index'].append(i)
+            par['Size'].append(np.sum(image))
+            if muon_evt[1]['RingComp'] is not None:
+                par['RingComp'].append(muon_evt[1]['RingComp'].ring_completeness)
+            else:
+                par['RingComp'].append(-1)
 
+    tab = index.to_table()
+    tab.write("Muon_search_result.fits",format="fits")
     print("Found "+str(len(index)+" muons"))
 
 
